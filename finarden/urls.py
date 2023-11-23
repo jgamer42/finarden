f8a2dc68b7cc94d112c1a78153056f2e794b4ba4
JeankBraham <juan.garcia4@utp.edu.co>
2023-11-23 12:45:09 -0500

    Implementación del conversor de divisas
    Se creó un conversor sencillo para la aplicación, el cual contiene algunas divisas principales como:
    - Peso colombiano (COP)
    - Dólar estadounidense (USD)
    - Euro (EUR)
    - Yen japonés (JPY)
    - Libra esterlina (GBP)
    - Dólar australiano (AUD)
    - Dólar canadiense (CAD)
    - Franco suizo (CHF)
    - Yuan chino (CNY)
    - Peso argentino (ARS)
    - Real brasileño (BRL)
    - Bolívar soberano (VES)

--- finarden/urls.py
+++ finarden/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from users import views
 
 urlpatterns = [
     path('home', admin.site.urls,name='home'),
-    path('users',include('users.urls'))
+    path('users',include('users.urls')),
+    path('converter', views.converter, name='converter')
 ]
